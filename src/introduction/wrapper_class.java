867da109f1dbab4eee6d7419dc610a6082fcafbd
Debang chowdhury <sambhuchowdhury512@gmail.com>
2025-08-08 14:33:12 +0530

    Hello World

---
+++ src/introduction/wrapper_class.java
@@ -0,0 +1,45 @@
+package introduction;
+
+public class wrapper_class {
+    public static void main(String[] args){
+        int c = 10;
+        Integer num = 50; // wrapper class
+        System.out.println(num);
+        // This is a wrapper class , means Converting a primitive into an object
+        // here num is an object
+        // num objectifies Integer
+
+        Integer a = 10;
+        Integer b = 50; // It will not Swap as Integer is a Final keyword
+        // Final prevents the constant from being modified a premetive datatype , it acts as a constant
+
+        swap(a, b);
+        System.out.println(a + " " + b);
+
+        // moving this code inside main so it runs
+        final a kuswaha_debang = new a();// It is permited as it is non premetive datatype but it cant allow you to reasign
+        kuswaha_debang.name = "Other_name";
+
+
+    }
+
+    static void swap(Integer a , Integer b){
+        Integer temp = a;
+        a = b;
+        b = temp;
+    }
+}
+
+// Separate top-level class
+class a {
+    final int num = 10; // Initialized directly, cannot be changed later , and cannot preocced without initiallization
+    String name;
+
+    public a(String name){
+        this.name = name;
+    }
+
+    a(){
+        this("Debang_Kuswaha");
+    }
+}
