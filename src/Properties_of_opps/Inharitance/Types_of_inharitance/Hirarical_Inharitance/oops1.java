1eac2b87a6384c2509974639afa119f8d1da6b43
Debang chowdhury <sambhuchowdhury512@gmail.com>
2025-08-11 15:57:44 +0530

    Hello World

---
+++ src/Properties_of_opps/Inharitance/Types_of_inharitance/Hirarical_Inharitance/oops1.java
@@ -0,0 +1,12 @@
+package Properties_of_opps.Inharitance.Types_of_inharitance.Hirarical_Inharitance;
+
+import Properties_of_opps.Inharitance.Superkeyword_privatekeyword.BoxWeight;
+
+public class oops1 {
+    //one class is inherited by many class it will access upto down not down to up
+
+
+    public class BoxColor extends BoxWeight {
+
+    }
+}
---
+++ src/Properties_of_opps/Inharitance/Types_of_inharitance/Hybrid_Inharitance/oops1.java
@@ -0,0 +1,13 @@
+package Properties_of_opps.Inharitance.Types_of_inharitance.Hybrid_Inharitance;
+
+public class oops1 {
+    //combination of single and multiple inheritance
+    //As multiple inheritance is present so like multilevel inheritance hybrid inheritance is not present in JAVA
+    // we can do by help of Interface
+    public static void main(String[] args) {
+        System.out.println("combination of single and multiple inheritance\n" +
+                "    As multiple inheritance is present so like multilevel inheritance hybrid inheritance is not present in JAVA\n" +
+                "     we can do by help of Interface");
+    }
+}
+
