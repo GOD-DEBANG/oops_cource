1611bb95dc50711ccef1b6adc9073759ee8ce5e8
Debang chowdhury <sambhuchowdhury512@gmail.com>
2025-08-12 21:19:58 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Properties_of_opps/Inharitance/Superkeyword_privatekeyword/Box.java
    #	src/Properties_of_opps/Inharitance/Superkeyword_privatekeyword/BoxWeight.java
    #	src/Properties_of_opps/Inharitance/Superkeyword_privatekeyword/Main.java

---
+++ src/Properties_of_opps/Abstractions/one.java
@@ -0,0 +1,11 @@
+package Properties_of_opps.Abstractions;
+
+public class one {
+    //Hiding the unnecessary  details and showing valuable information example:  get the element or  print the element etc.
+    // here we need not know how the element is getting or printing
+    // in simple word we can say abstraction solve design level issue
+    // abstraction provides some outside method that is tobe used
+    // it focuses on the external world
+    //abstraction is a process of gaining information
+
+}
---
+++ src/Properties_of_opps/Encapsulation/one.java
@@ -0,0 +1,17 @@
+package Properties_of_opps.Encapsulation;
+
+public class one {
+    //Wrapping up the implementation of the data member and data method in the class , that's mean encapsulation
+// in simple term we can say encapsulation solves implementation level issue
+    //encapsulation mean it is the code that solve the abstraction level or design level problem
+    // example : if any one call static or println the code behind the println will be the encapsulation
+    // this code is responsible for the implementation
+    // it hides the code into single entity internally and protected from outer world
+    // it focuses on the internal world
+    // encapsulation is the process of containing information
+    // Encapsulation focuses on hiding complexity of the system
+    // data hiding focus on data security ex: getters and setters
+    // data hiding should be private cannot be accessed
+    //but in encapsulation data can be private public void anything
+    // encapsulation is a part of data hiding
+}
